546111f30ad038bd5320c4612f739c64ba090531
Craig Weber <crgwbr@gmail.com>
2021-05-27 09:50:26 -0400

    Remove deprecated providing_args argument to signals

--- src/cybersource/signals.py
+++ src/cybersource/signals.py
@@ -1,5 +1,10 @@
 import django.dispatch
 
-pre_build_get_token_request = django.dispatch.Signal(providing_args=["extra_fields", "request", "order", "method_key"])
-pre_build_auth_request = django.dispatch.Signal(providing_args=["extra_fields", "request", "order", "token", "method_key"])
-received_decision_manager_update = django.dispatch.Signal(providing_args=["order", "transaction", "update"])
+# providing_args=["extra_fields", "request", "order", "method_key"]
+pre_build_get_token_request = django.dispatch.Signal()
+
+# providing_args=["extra_fields", "request", "order", "token", "method_key"]
+pre_build_auth_request = django.dispatch.Signal()
+
+# providing_args=["order", "transaction", "update"]
+received_decision_manager_update = django.dispatch.Signal()
